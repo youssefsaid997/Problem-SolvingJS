5fbc1c0f7410a2999b6c598459cc0ac69382e8bc
youssefsaid997 <57658169+youssefsaid997@users.noreply.github.com>
2023-10-11 05:56:44 +0300

    fix: cleaning the code in countLetters function

--- countLetters.js
+++ countLetters.js
@@ -1,16 +1,27 @@
 // function ("HeLlo" , "L") ==> 2
 
 function countComparedLetter(word, letter) {
-	const wordArray = word.toLowerCase().split('');
-	const lowerCaseLetter = letter.toLowerCase();
+	const letterForComparing = getLowerCasedLetters(letter);
+	const wordForComparing = getWordArray(word);
 
+	return compareWordWithLetter(wordForComparing, letterForComparing);
+}
+
+function getWordArray(word) {
+	return getLowerCasedLetters(word).split('');
+}
+
+function getLowerCasedLetters(letter) {
+	return letter.toLowerCase();
+}
+
+function compareWordWithLetter(wordArray, letter) {
 	let count = 0;
 	for (let index = 0; index < wordArray.length; index++) {
-		if (lowerCaseLetter === wordArray[index]) {
+		if (letter === wordArray[index]) {
 			count++;
 		}
 	}
-
 	return count;
 }
 
