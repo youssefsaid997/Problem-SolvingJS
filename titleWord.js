e629c8f8359b4d828b2e6857d973f4d50b94d994
youssefsaid997 <57658169+youssefsaid997@users.noreply.github.com>
2023-10-11 08:10:23 +0300

    uploading title case problem

---
+++ titleWord.js
@@ -0,0 +1,24 @@
+//function should return Title Case values
+//function("hELlo wORlD") => Hello World
+
+function getWordsFromSentence(sentence) {
+	return sentence.toLowerCase().split(' ');
+}
+
+function capitalizeFirstLetter(word) {
+	const firstLetter = word[0].toUpperCase();
+	return firstLetter + word.slice(1);
+}
+
+function capitalizeWordsInArray(array) {
+	return array.map((word) => {
+		return capitalizeFirstLetter(word);
+	});
+}
+
+function getTitleCase(sentence) {
+	const wordsFromSentence = getWordsFromSentence(sentence);
+	const capitalizedWordsInArray = capitalizeWordsInArray(wordsFromSentence);
+	return capitalizedWordsInArray.join(' ');
+}
+console.log(getTitleCase('hello my name is youssef'));
